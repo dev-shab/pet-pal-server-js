d53693e71f23b3a3f5a36feeb184f41c2943f1fc
dev-shab <shabreeshnair1998@gmail.com>
2025-09-17 22:03:09 +0530

    PETPAL-3: Dotenv setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   return res.status(200).send("Hello from PET PAL");
 });
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
