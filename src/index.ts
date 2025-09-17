9b7cfc38238d654c3b307943f1b5197d5927a820
dev-shab <shabreeshnair1998@gmail.com>
2025-09-17 21:41:36 +0530

    PETPAL-2: Setup basic express app (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello from PET PAL");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.status(200).send("Hello from PET PAL");
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
