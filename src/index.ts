d4d9ecc7da72f4b8e149666014403d6344c1d58a
dev-shab <shabreeshnair1998@gmail.com>
2025-09-17 22:17:08 +0530

    PETPAL-3: Mongo db connect, prettier and alias setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
 import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+if (process.env.MONGOOSE_CONNECTION_STRING) {
+  mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING).then(() => {
+    console.log("connected to DB");
+  });
+}
 
 app.get("/", (req, res) => {
   return res.status(200).send("Hello from PET PAL");
